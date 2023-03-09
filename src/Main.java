2438fb42c81d0754852b7d4c4b5e3bf4c1b5795d
Pavel Minin <minin09092002@mail.ru>
2023-03-09 23:22:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> result = new ArrayList<>();
+        for (Integer x : intList) {
+            if (x > 0 && x % 2 == 0) {
+                result.add(x);
+            }
+        }
+        result.sort(Integer::compareTo);
+        System.out.println(result);
+    }
+}
---
+++ src/StreamMain.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        list.stream().filter(x -> x > 0 && x % 2 == 0).sorted(Integer::compareTo)
+                .forEach(x -> System.out.print(x + " "));
+    }
+}
